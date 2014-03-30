302eaac49b22670b9aba46fc60daec74d5c8e467
Kyle Hodgson <khodgson@thoughtworks.com>
2014-03-30 08:51:55 -0400

    fixing tests

--- spec/todont_spec.js
+++ spec/todont_spec.js
@@ -7,6 +7,7 @@ describe('toDontList app', function() {
     var greeting = element(by.id('greeting'));
     expect(greeting.getText()).toEqual('Do Not Do List');
   });
+
   it('should be displaying starter items', function() {
     var firstItemName = element(by.repeater('item in items').row(0).column('title'));
     expect(firstItemName.getText()).toEqual("Item one!");
