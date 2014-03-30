089cd3235a77c460f93e03533a7678114048fdc9
Kyle Hodgson <khodgson@thoughtworks.com>
2014-03-30 16:22:43 -0400

    more testing of controllers

--- spec/todont_test.js
+++ spec/todont_test.js
@@ -7,5 +7,17 @@ describe('ToDontList',function() {
   it("can return items", function() {
     var result = Acme.ListItems().getItems();
     expect(result[0].title).toBe("Item one!");
-  })
+  });
+  
+  it("updates scope with items", function() {
+    var scope = {};
+    var mockItemsService = {
+      getItems: function() {
+        return [{ title: "Test item"}];
+      }
+    };
+    new Acme.ListCtrl(scope, mockItemsService);
+    expect(scope.items[0].title).toEqual("Test item");
+  });
+  
 });
