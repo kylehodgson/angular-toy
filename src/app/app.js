7ccb3b33413c47b37d040be14dc9c1209308b785
Kyle Hodgson <khodgson@thoughtworks.com>
2014-03-31 17:48:43 -0400

    protecting app against minification

--- src/app/app.js
+++ src/app/app.js
@@ -2,6 +2,6 @@ Acme = typeof Acme === "undefined"? {} : Acme;
 
 Acme.toDontList = angular.module("toDontList", []);
 Acme.toDontList.factory("ListItems", Acme.ListItems);
-Acme.toDontList.controller("ListCtrl", Acme.ListCtrl); 
+Acme.toDontList.controller("ListCtrl", ["ListItems", Acme.ListCtrl]); 
 
 
