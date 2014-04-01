f19ed19112741799e1aadede7a9874e41ea913a0
Avishek Sen Gupta <avishek.sen.gupta@gmail.com>
2014-03-31 23:43:35 -0400

    No need for loaded convention for sync case - <Mojo>

--- src/app/controllers/list-controller.js
+++ src/app/controllers/list-controller.js
@@ -3,7 +3,6 @@ Acme = typeof Acme === "undefined"? {} : Acme;
 Acme.ListCtrl = function($scope, ListItems) {
   $scope.items = ListItems.getItems();
   $scope.randomString = "Birdie";
-  this.loaded = Q.fcall(function() {});
 
   $scope.changeData = function() {
   	console.log("I was fired");
